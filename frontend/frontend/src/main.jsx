7a60ca5f4311b4c747596d9211be7e15ffaa7d78
Mrinal Mayank <krish404147@gmail.com>
2026-06-06 18:16:55 +0530

    Add .jsx extension to import

--- frontend/frontend/src/main.jsx
+++ frontend/frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import { AppContextProvider } from './context/APPContext'
+import { AppContextProvider } from './context/APPContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
